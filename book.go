34c3b96df7040508e9b4ce38d51efd93c774cb6a
Ethan Marshall <ethan@ethanjmarshall.co.uk>
2023-10-24 19:56:51 +0100

    Add stage URL parsing

--- book.go
+++ book.go
@@ -1,12 +1,87 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"net/url"
+	"regexp"
 	"strconv"
+	"strings"
 
 	"github.com/ejv2/prepper/data"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
+
+// Regexes for matching against parameters.
+var (
+	matchItems = regexp.MustCompile("^qty_.*")
+	matchExtra = regexp.MustCompile("^eqty_.*")
+)
+
+// ItemInformation is the set of information submitted for use in the next
+// stage of the booking wizard. Only the item ID, quantity and importance are
+// quaranteed to be filled in.
+type ItemInformation []data.EquipmentSet
+
+// NewItemInformation parses a new ItemInformation set from a request's query
+// parameters.
+func NewItemInformation(r *http.Request) (ItemInformation, error) {
+	qs := r.URL.Query()
+
+	inf := make(ItemInformation, len(qs))
+	for param := range qs {
+		if matchItems.MatchString(param) || matchExtra.MatchString(param) {
+			segs := strings.Split(param, "_")
+			if len(segs) != 2 {
+				return inf, fmt.Errorf("parse item information: parse ID: invalid syntax")
+			}
+
+			sid := segs[1]
+			lid, err := strconv.ParseUint(sid, 10, 32)
+			id := uint(lid)
+			if err != nil {
+				return inf, fmt.Errorf("parse item information: parse ID: %w", err)
+			}
+
+			lqty, err := strconv.ParseUint(qs.Get(param), 10, 32)
+			qty := uint(lqty)
+			if err != nil {
+				return inf, fmt.Errorf("parse item information: parse quantity: %w", err)
+			}
+
+			inf = append(inf, data.EquipmentSet{
+				Quantity:  qty,
+				Important: !matchExtra.MatchString(param),
+				ItemID:    id,
+				Item: data.EquipmentItem{
+					Model: &gorm.Model{ID: id},
+				},
+			})
+		}
+	}
+
+	return inf, nil
+}
+
+// Next returns the URL of the next stage in the wizard based on this
+// information.
+func (i ItemInformation) Next(activity uint) string {
+	url := url.URL{}
+	url.Path = fmt.Sprint("/book/", activity, "/submit")
+
+	for _, item := range i {
+		base := "qty_"
+		if !item.Important {
+			base = "e" + base
+		}
+
+		key := fmt.Sprint(base, item.Item.ID)
+		url.Query().Add(key, fmt.Sprint(item.Quantity))
+	}
+
+	return url.String()
+}
 
 // handleBook is the handler for "/book/"
 //
@@ -83,4 +158,38 @@ func handleBookActivity(c *gin.Context) {
 // entering timing and location information. On submission, the booking is
 // created and all required rows are copied.
 func handleBookTimings(c *gin.Context) {
+	s := Sessions.Start(c)
+	defer s.Update()
+
+	ddat, err := NewDashboardData(s)
+	if err != nil {
+		internalError(c, err)
+		return
+	}
+
+	sid := c.Param("activity")
+	lid, err := strconv.ParseUint(sid, 10, 32)
+	if err != nil {
+		c.String(http.StatusBadRequest, "Bad Activity ID")
+		return
+	}
+	id := uint(lid)
+
+	act, err := data.GetActivity(Database, id)
+	if err != nil {
+		internalError(c, err)
+		return
+	}
+
+	set, err := NewItemInformation(c.Request)
+	if err != nil {
+		c.String(http.StatusBadRequest, "Invalid Paramater Format Format: %s", err)
+	}
+
+	dat := struct {
+		DashboardData
+		Activity data.Activity
+		Items    ItemInformation
+	}{ddat, act, set}
+	c.HTML(http.StatusOK, "book-timings.gohtml", dat)
 }
