253a3d6b38b0e90401882d567cc08f20a08c74bf
Ethan Marshall <ethan@ethanjmarshall.co.uk>
2023-10-24 22:51:31 +0100

    Add maintenance mode structures
    Will be needed at some point for cleaning up temp activities.

---
+++ maintenance/maintenance.go
@@ -0,0 +1,57 @@
+package maintenance
+
+import (
+	"errors"
+	"sync"
+	"time"
+)
+
+// A Manager is a thread-safe structure which is able to be checked for
+// maintenance mode.
+type Manager struct {
+	*sync.RWMutex
+	is      bool
+	entered time.Time
+}
+
+// Is returns if the site is currently in maintenance mode.
+func (m Manager) Is() bool {
+	m.RLock()
+	defer m.RUnlock()
+
+	return m.is
+}
+
+// Enter waits for an exclusive lock on the manager before entering maintenance
+// mode. This will cause all other clients to block before entering the mode.
+// If, upon acquiring the lock, the manager is already in maintenance mode, an
+// error is returned.
+//
+// Essentially, if this function returns with a nil error, a request may treat
+// it as though it has an exclusive lock on the site.
+func (m Manager) Enter() error {
+	m.Lock()
+	defer m.Unlock()
+
+	if m.is {
+		return errors.New("already in maintenance mode")
+	}
+
+	m.is = true
+	m.entered = time.Now()
+	return nil
+}
+
+// Exit waits for an exclusive lock on the manager before exiting maintenance.
+// If the site is not currently in maintenance, Exit panics.
+func (m Manager) Exit() {
+	m.Lock()
+	defer m.Unlock()
+
+	if !m.is {
+		panic("request exited maintenance mode when not entered")
+	}
+
+	m.is = false
+	m.entered = time.Time{}
+}
---
+++ maintenance/middleware.go
@@ -0,0 +1,33 @@
+package maintenance
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+const defaultErrorMessage = `
+Prepper on this server is temporarily unavailable for maintenance. Please check back in 2-5 minutes.
+
+We apologize for any inconvenience.
+
+(If this page shows for an extended period, please contact your system administrator)
+`
+
+// defaultError is the default handler to call in case of error.
+func defaultError(c *gin.Context) {
+	c.AbortWithStatus(http.StatusServiceUnavailable)
+	c.String(http.StatusServiceUnavailable, defaultErrorMessage)
+}
+
+func Middleware(m Manager) gin.HandlerFunc {
+	return MiddlewareWithHandler(m, defaultError)
+}
+
+func MiddlewareWithHandler(m Manager, onError gin.HandlerFunc) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if m.Is() {
+			onError(c)
+		}
+	}
+}
