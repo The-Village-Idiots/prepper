5d2aea7de42a81abab9fa5b25e9d3023d6f547a0
Ethan Marshall <ethan@ethanjmarshall.co.uk>
2023-10-25 21:32:54 +0100

    Add some documentation about time zones

--- data/data.go
+++ data/data.go
@@ -1,4 +1,7 @@
 // Package data contains the data structure definitions for prepper. It is
 // responsible for interactions with GORM and the database's structures in
 // general.
+//
+// Unless otherwise specified, all dates and times are stored in UTC and
+// converted where necessary.
 package data
